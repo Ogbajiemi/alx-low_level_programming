9bb380487e78261dbc15be227a58c8186c426863
ogbaji ugo <emmibrain003@gmail.com>
2023-02-21 14:45:31 -0800

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char my_char;
+
+	for (my_char = 'a'; my_char <= 'z'; my_char++)
+	{
+		_putchar(my_char);
+	}
+		_putchar('\n');
+
+		
+}
