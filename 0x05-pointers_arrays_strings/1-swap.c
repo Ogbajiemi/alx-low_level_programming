97611ef96bb9fbce9003163a0ad7ed706f46f418
ogbaji ugo <emmibrain003@gmail.com>
2023-02-27 13:24:51 -0800

    Dint swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: pointer to first value
+ *
+ * @b: pointer to second value
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p;
+
+	p = *a;
+	*a = *b;
+	*b = p;
+}
