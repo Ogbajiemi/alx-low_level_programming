6e618c4a83d22377e72815624d0a8d5d3fe940c4
ogbaji ugo <emmibrain003@gmail.com>
2023-03-02 14:12:19 -0800

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int p, k;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		for (k = 0; k < 52; k++)
+		{
+			if (s[p] == data1[k])
+			{
+				s[p] = datarot[k];
+				break;
+			}
+		}
+	}
+	return (s);
+}
