860c43e5ccdfd97f773db5c7b498c79abd40f238
ogbaji ugo <emmibrain003@gmail.com>
2023-03-07 13:06:50 -0800

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,30 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
-	if (needle == 0)
-		return (haystack);
-
-	while (*haystack)
+	for (; *haystack != '\0'; haystack++)
 	{
-		i = 0;
+		char *h = haystack;
+		char *n = needle;
 
-		if (haystack[i] == needle[i])
+		while (*h == *n && *n != '\0')
 		{
-			do {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-
-				i++;
-			} while (haystack[i] == needle[i]);
-
+			h++;
+			n++;
 		}
 
-		haystack++;
-
+		if (*n == '\0')
+			return (haystack);
 	}
 
-	return ('\0');
-
+	return (0);
 }
