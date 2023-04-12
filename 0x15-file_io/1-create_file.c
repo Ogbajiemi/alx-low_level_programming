6016811f430e02d3c285a384bc4ecd36167a38e5
ogbaji ugo <emmibrain003@gmail.com>
2023-04-12 09:24:40 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,32 +11,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fp;
 	int len = 0;
-	int i;
+	int fp;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
 	if (text_content == NULL)
 	{
-		text_content = " ";
+		text_content = "";
 	}
 
-	for (i = 0; text_content[i] != '\0'; i++)
+	for (len = 0; text_content[len] != '\0'; len++)
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	write(fp, text_content, len);
 
-	if (fp == -1)
-	{
-		return (-1);
-	}
-
-	close(fp);
 
 	return (1);
 }
