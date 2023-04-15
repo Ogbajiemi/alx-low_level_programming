74922b4a16331e25e212adf938b5eb6fd423395a
ogbaji ugo <emmibrain003@gmail.com>
2023-04-14 17:36:38 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,40 +14,39 @@ int main(int argc, char *argv[])
 	int src_file, dest_file, rd = 1024, wrt = 0;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n");
-		exit(97);
-
+		dprintf(STDERR_FILENO, "Usage: cp file_from dest_file\n"), exit(97);
 	src_file = open(argv[1], O_RDONLY);
 	if (src_file == -1 || rd == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-
-	dest_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
-			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (dest_file == 1 || wrt == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
-		close(src_file);
-		exit(99);
-
+	}
+	dest_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+		| S_IRGRP | S_IWGRP | S_IROTH);
+	if (dest_file == -1 || wrt == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(src_file), exit(99);
+	}
 	while (rd == 1024)
 	{
 		rd = read(src_file, bf, 1024);
 		if (rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
-
+		}
 		wrt = write(dest_file, bf, rd);
 		if (rd > wrt)
-			dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
-			exit(99);
-
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	if (close(src_file == -1))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_file);
-		exit(100);
 
-	if (close(dest_file == -1))
+	if (close(src_file) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_file), exit(100);
+
+	if (close(dest_file) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_file), exit(100);
 
 	return (0);
 }
+
